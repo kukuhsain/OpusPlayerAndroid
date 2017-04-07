ec664991da5c6ee13a045838019f211406a6fdff
Kukuh <kukuhsain@gmail.com>
2017-04-08 05:50:33 +0700

    Add simple file picker functionality to MainActivity

--- app/src/main/java/com/kukuhsain/opusplayer/MainActivity.java
+++ app/src/main/java/com/kukuhsain/opusplayer/MainActivity.java
@@ -1,13 +1,58 @@
 package com.kukuhsain.opusplayer;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int PICK_OPUS_FILE_REQUEST = 1000;
+
+    @BindView(R.id.tv_file_name) TextView tvFileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        EasyPermissions.requestPermissions(this,
+                "Opus Player need a permission to be able to get opus file from your storage",
+                PICK_OPUS_FILE_REQUEST,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+    }
+
+    @OnClick(R.id.btn_pick)
+    public void pickOpusFile() {
+        Intent intent = new Intent();
+        intent.setType("*/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_OPUS_FILE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_OPUS_FILE_REQUEST && resultCode == RESULT_OK && data != null) {
+            Uri fileUri = data.getData();
+            tvFileName.setText(fileUri.getPath());
+            tvFileName.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 }
